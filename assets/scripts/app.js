c8d17cb18eefac6ab1aa1c09eb070f2d37451c5e
Luis Ramirez <sdaewa22@gmail.com>
2020-01-23 18:19:38 +0100

    anon function for modal

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,3 +1,7 @@
 const addMovieModal = document.getElementById('add-modal'); // selecting HTML tag by id
 // const addMovieModal = document.querySelector('#add-modal');
-const startAddButton = document.querySelector('header button');
+const startAddButton = document.querySelector('header button'); // selecting button tag in header
+
+startAddButton.addEventListener('click', () => {
+    addMovieModal.classList.toggle('visible')
+}); // on click toggle modal
