bd0bf171205efe0b5ea3773b052f0f0263a1d72d
Luis Ramirez <sdaewa22@gmail.com>
2020-01-24 15:25:11 +0100

    another way of validating input done, also I am on a train

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -28,15 +28,22 @@ const addMovieHandler = () => {
     const titleValue = userInputs[0].value; // value property
     const imageValue = userInputs[1].value;
     const ratingValue = userInputs[2].value;
-    if (userInputs === '') {
-        alert('Please enter Title, Image and Rating');
-    } else if (!titleValue) {
-        alert('Please enter Title');
-    } else if (!imageValue) {
-        alert('Please enter Image');
-    } else if (ratingValue <= 0) {
-        alert('Please enter Rating');
-    }
+    // if (userInputs === '') {
+    //     alert('Please enter Title, Image and Rating');
+    // } else if (!titleValue) {
+    //     alert('Please enter Title');
+    // } else if (!imageValue) {
+    //     alert('Please enter Image');
+    // } else if (ratingValue <= 0) {
+    //     alert('Please enter Rating');
+    // }
+    if (titleValue.trim() === '' ||
+        imageValue.trim() === '' ||
+        ratingValue.trim() <= 0 ||
+        +ratingValue < 1 ||
+        +ratingValue > 5) {
+        alert('Please enter valid values');
+    };
 };
 
 startAddButton.addEventListener('click', toggleMovieModalHandler);
